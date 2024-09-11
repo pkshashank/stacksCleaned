bc89f724a613076a92409deafd6005962aa7aacc
Shashank Pathak <pathakshashank97@gmail.com>
2024-09-11 13:51:27 +0100

    Corrected

--- LexiconMaker.py
+++ LexiconMaker.py
@@ -21,44 +21,47 @@ def POSFinder(infileName,outfileName):
             index = word.find("_")
             tag = word[index+1:]
             lexeme = word[:index]
-            setPOS.add(tag + " " + lexeme)
+            setPOS.add(tag + " " + lexeme.lower())
             if tag == "CC":
-                Conjunctions.add(lexeme)
+                Conjunctions.add(lexeme.lower())
             elif tag == "CD":
-                CardinalNumbers.add(lexeme)
+                CardinalNumbers.add(lexeme.lower())
             elif tag == "DT":
-                Determiners.add(lexeme)
+                Determiners.add(lexeme.lower())
             elif tag == "FW":
-                ForeignWords.add(lexeme)
+                ForeignWords.add(lexeme.lower())
             elif tag == "IN":
-                Prepositions.add(lexeme)
+                Prepositions.add(lexeme.lower())
             elif tag == "JJ" or tag == "JJR" or tag == "JJS":
-                Adjectives.add(lexeme)
+                Adjectives.add(lexeme.lower())
             elif tag == "MD":
-                Modals.add(lexeme)
+                Modals.add(lexeme.lower())
             elif tag == "NN" or tag == "NNS" or tag == "NNP" or tag == "NNPS":
-                Nouns.add(lexeme)
+                Nouns.add(lexeme.lower())
             elif tag == "PDT":
-                Predeterminers.add(lexeme)
+                Predeterminers.add(lexeme.lower())
             elif tag == "PRP" or tag == "PRP$":
-                Pronouns.add(lexeme)
+                Pronouns.add(lexeme.lower())
             elif tag == "RB" or tag == "RBR" or tag == "RBS":
-                Adverbs.add(lexeme)
+                Adverbs.add(lexeme.lower())
             elif tag == "VB" or tag == "VBD" or tag == "VBG" or tag == "VBN" or tag == "VBP" or tag == "VBZ":
-                Verbs.add(lexeme)
+                Verbs.add(lexeme.lower())
     infile.close()
-    outfile.write("Conjunctions: " + str(len(Conjunctions)) + "\n")
-    outfile.write("Cardinal Numbers: " + str(len(CardinalNumbers)) + "\n")
-    outfile.write("Determiners: " + str(len(Determiners)) + "\n")
-    outfile.write("Foreign Words: " + str(len(ForeignWords)) + "\n")
-    outfile.write("Prepositions: " + str(len(Prepositions)) + "\n")
-    outfile.write("Adjectives: " + str(len(Adjectives)) + "\n")
-    outfile.write("Modals: " + str(len(Modals)) + "\n")
-    outfile.write("Nouns: " + str(len(Nouns)) + "\n")
-    outfile.write("Predeterminers: " + str(len(Predeterminers)) + "\n")
-    outfile.write("Pronouns: " + str(len(Pronouns)) + "\n")
-    outfile.write("Adverbs: " + str(len(Adverbs)) + "\n")
-    outfile.write("Verbs: " + str(len(Verbs)) + "\n\n\n")
+    outfile.write("Total Words: " + str(len(setPOS)) + "\n")
+    outfile.write("Conjunctions (CC): " + str(len(Conjunctions)) + "\n")
+    outfile.write("Cardinal Numbers (CD): " + str(len(CardinalNumbers)) + "\n")
+    outfile.write("Determiners (DT): " + str(len(Determiners)) + "\n")
+    outfile.write("Foreign Words (FW): " + str(len(ForeignWords)) + "\n")
+    outfile.write("Prepositions (IN): " + str(len(Prepositions)) + "\n")
+    outfile.write("Adjectives (JJ, JJR, JJS): " + str(len(Adjectives)) + "\n")
+    outfile.write("Modals (MD): " + str(len(Modals)) + "\n")
+    outfile.write("Nouns (NN, NNS, NNP, NNPS): " + str(len(Nouns)) + "\n")
+    outfile.write("Predeterminers (PDT): " + str(len(Predeterminers)) + "\n")
+    outfile.write("Pronouns (PRP, PRP$): " + str(len(Pronouns)) + "\n")
+    outfile.write("Adverbs (RB, RBR, RBS): " + str(len(Adverbs)) + "\n")
+    outfile.write("Verbs (VB, VBD, VBG, VBN, VBP, VBZ): " + str(len(Verbs)) + "\n\n")
+    
+    outfile.write("Lexicon: \n")
     for elements in setPOS:
         toWrite.append(elements + "\n")
     toWrite.sort()
